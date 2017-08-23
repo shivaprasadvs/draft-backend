8d1423affa957dbbc6dd4fa40b16a52c6f00a535
Shiva Prasad <svisw1@monsanto.com>
2017-08-23 10:49:34 -0500

    first commit

--- routes/addPlayer.js
+++ routes/addPlayer.js
@@ -2,13 +2,21 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Player = require('../models/Player');
+mongoose.Promise = global.Promise;
 mongoose.createConnection('mongodb://localhost:27017/players');
 
 router.post('/', function(req, res) {
 var db = mongoose.connection;
 
 var player = new Player(req.body);
- res.send("connected");
+player.save(function (err) {
+  if (err) {
+    res.send(err); 
+  } 
+    res.send('meow');
+  
+});
+ 
 });
 
 module.exports = router;
