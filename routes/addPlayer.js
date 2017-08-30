99112641b32376252d5c1782f15eb88e97c2a34a
Shiva Prasad <svisw1@monsanto.com>
2017-08-30 13:07:28 -0500

    AWS lb

--- routes/addPlayer.js
+++ routes/addPlayer.js
@@ -6,11 +6,7 @@ mongoose.Promise = global.Promise;
 
 
 router.post('/', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-var player = new Player(req.body);
+ var player = new Player(req.body);
 player.save(function (err) {
   if (err) {
     res.send(err); 
--- routes/allPlayers.js
+++ routes/allPlayers.js
@@ -6,10 +6,6 @@ mongoose.Promise = global.Promise;
 
 
 router.get('/', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
     Player.find({}).exec(function (err, players) {
   if (err) {
     res.send(err); 
