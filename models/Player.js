6eaf1a194a81eb7c0b744a62c5a93f475754bdf7
Shiva Prasad <svisw1@monsanto.com>
2017-08-30 13:49:06 -0500

    AWS lb

--- models/Player.js
+++ models/Player.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 //var connection = mongoose.createConnection('mongodb://localhost:27017/players')
-var connection = mongoose.createConnection('mongodb://admin:Getin12345xy@ds161873.mlab.com:61873/mcldraft2017/players');
+var connection = mongoose.createConnection('mongodb://admin:Getin12345xy@ds111204.mlab.com:11204/mcldraft/players');
+
 var playerSchema = mongoose.Schema({
     auth_id: String,
     auth_service: String,
